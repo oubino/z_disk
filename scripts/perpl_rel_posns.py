8c11152c42e3f3549b721faa456add74c7ae0d1a
Oliver (linux) <scou@leeds.ac.uk>
2025-10-08 15:37:52 +0100

    Relative positions now automatically calculates localisation precision... and deals with files not having enough relative distances...

--- scripts/perpl_rel_posns.py
+++ scripts/perpl_rel_posns.py
@@ -2,7 +2,9 @@ import argparse
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import polars as pl
+import seaborn as sns
 
 from perpl.io import plotting
 from perpl.relative_positions import main as calculate_relative_positions
@@ -89,43 +91,80 @@ def main(argv=None):
 
     d_values_list = []
 
-    if args.visualise:
+    # calculate localisation precision
 
-        x_prec = []
-        y_prec = []
-        z_prec = []
-
-        for file in files:
-
-            file_path = os.path.join(input_folder, file)
-
-            df = pl.read_csv(file_path)
-
-            x_prec.append(df["Group Sigma X Pos"])
-            y_prec.append(df["Group Sigma Y Pos"])
-            z_prec.append(df["Group Sigma Z"])
-
-        x_prec = np.concatenate(x_prec)
-        y_prec = np.concatenate(y_prec)
-        z_prec = np.concatenate(z_prec)
-
-        x_prec = [float(x.lstrip(" ")) for x in x_prec]
-        y_prec = [float(y.lstrip(" ")) for y in y_prec]
-        z_prec = [float(z.lstrip(" ")) for z in z_prec]
-
-        plt.hist(x_prec, bins=20, histtype="step", label="x prec")
-        plt.hist(y_prec, bins=20, histtype="step", label="y prec")
-        plt.hist(z_prec, bins=20, histtype="step", label="z prec")
-        plt.legend()
-        plt.show()
-
-    mean_precision_list = []
+    x_prec = []
+    y_prec = []
+    z_prec = []
 
     for file in files:
 
         file_path = os.path.join(input_folder, file)
 
         df = pl.read_csv(file_path)
+
+        x_prec.append(df["Group Sigma X Pos"])
+        y_prec.append(df["Group Sigma Y Pos"])
+        z_prec.append(df["Group Sigma Z"])
+
+    x_prec = np.concatenate(x_prec)
+    y_prec = np.concatenate(y_prec)
+    z_prec = np.concatenate(z_prec)
+
+    x_prec = [float(x.lstrip(" ")) for x in x_prec]
+    y_prec = [float(y.lstrip(" ")) for y in y_prec]
+    z_prec = [float(z.lstrip(" ")) for z in z_prec]
+
+    pd_df = pd.DataFrame({
+        "X (nm)": x_prec,
+        "Y (nm)": y_prec,
+        "Z (nm)": z_prec,
+    })
+
+
+    # Use Arial font and set global font sizes
+    plt.rcParams.update({
+        "axes.titlesize": 16,      # title size
+        "axes.labelsize": 14,      # x/y label size
+        "xtick.labelsize": 12,     # x tick label size
+        "ytick.labelsize": 12,     # y tick label size
+        "legend.fontsize": 12,     # legend label size
+    })
+
+    plt.figure(figsize=(10,6))
+    plot = sns.histplot(
+            data=pd_df, 
+            element="step", 
+            stat="count", 
+            )
+
+    legend = plot.get_legend()
+    legend.set_title(None)
+    plt.xlim(0,50)
+    plt.xticks([0,5,10,15,20,25,30,35,40,45,50])
+    plt.xlabel("Localisation precision")
+    plt.ylabel("Localisation count")
+    hist_loc = os.path.join(output_folder, "loc_prec_histogram.svg")
+    plt.savefig(hist_loc, bbox_inches='tight', transparent=True)
+
+    print("Mean precision (pre-filtering)")
+    print(pd_df.mean())
+
+    print("Median precision (pre-filtering)")
+    print(pd_df.median())
+
+    mean_precision_list = []
+    dropped_files = []
+    input_locs = 0
+    output_locs = 0
+
+    for file in files:
+
+        file_path = os.path.join(input_folder, file)
+
+        df = pl.read_csv(file_path)
+
+        input_locs += len(df)
 
         df = df.filter(
             pl.col("Group Sigma X Pos").str.strip_chars_start(" ").cast(pl.Float64)
@@ -161,10 +200,7 @@ def main(argv=None):
             .mean()
             .item()
         )
-        mean_precision_list.append(
-            (mean_x_precision + mean_y_precision + mean_z_precision) / 3
-        )
-
+       
         # Based on lines 845-910 in perpl.relative_positions
 
         xyz_values = df[["x", "y", "z"]].to_numpy()
@@ -191,15 +227,22 @@ def main(argv=None):
         # and calculates distances.
         if len(d_values) > 0:
             d_values = get_vectors(d_values, dims=3)
+            d_values_list.append(d_values)
+            mean_precision_list.append((mean_x_precision + mean_y_precision + mean_z_precision) / 3)
+            output_locs += len(df)
         else:
-            raise ValueError("No distances within filter")
-
-        d_values_list.append(d_values)
+            print(f"No distances within filter for {file}")
+            dropped_files.append(file)
 
     d_values = np.concatenate(d_values_list, axis=0)
 
+    results_dir = os.path.join(folder, "perpl_relative_posns", f"{args.filter_distance}filterdistance_{args.localisation_precision_filter}precisionfilter")
+
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+
     info = {
-        "results_dir": os.path.join(folder, "perpl_relative_posns"),
+        "results_dir": results_dir,
         "short_names": False,
         "in_file_no_extension": f"all_z_disks_{args.localisation_precision_filter}precisionfilter",
     }
@@ -213,6 +256,9 @@ def main(argv=None):
         binsize=args.bin_size,
     )
 
+    # change so relative positions files saved dir higher than hists
+    info["results_dir"] = results_dir = os.path.join(folder, "perpl_relative_posns")
+
     ## Save relative positions and vector components.
     xyz_filename = save_relative_positions(
         d_values, args.filter_distance, dims=3, info=info, nns=args.nearest_neighbours
@@ -220,8 +266,14 @@ def main(argv=None):
 
     file_name = xyz_filename.replace("relpos", "locprec").rstrip(".csv") + ".txt"
 
+    print("Mean precision (after filtering): ", np.mean(mean_precision_list))
+
     np.savetxt(file_name, np.atleast_1d(np.mean(mean_precision_list)))
 
+    print("Dropped files: ", dropped_files)
+    print("Number of dropped files: ", len(dropped_files))
+    print("Number of retained files: ", len(files) - len(dropped_files))
+    print("% of localisations kept: ", 100*output_locs/input_locs)
 
 if __name__ == "__main__":
     main()
