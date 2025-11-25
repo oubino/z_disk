1ac6ada763024077ecfbb04d9da4bf5822de876e
Oliver (linux) <scou@leeds.ac.uk>
2025-11-25 10:37:40 +0000

    update script k1 to make figures/tables better

--- scripts/manuscript/script_K1.py
+++ scripts/manuscript/script_K1.py
@@ -10,14 +10,14 @@ import seaborn as sns
 output_folder = "manuscript_figures"
 
 table_protein = []
-table_xprec_med = []
-table_yprec_med = []
-table_zprec_med = []
+table_xprec_mean = []
+table_yprec_mean = []
+table_zprec_mean = []
 table_nzdisks = []
-table_locsperzdisk_med = []
-table_zdisksize_x_med = []
-table_zdisksize_y_med = []
-table_zdisksize_z_med = []
+table_locsperzdisk_mean = []
+table_zdisksize_x_mean = []
+table_zdisksize_y_mean = []
+table_zdisksize_z_mean = []
 
 for protein in ["ACTN2", "Z1Z2", "ZASP6"]:
 
@@ -64,24 +64,24 @@ for protein in ["ACTN2", "Z1Z2", "ZASP6"]:
 
     pd_df = pd.DataFrame(
         {
-            "X (nm)": x_prec,
-            "Y (nm)": y_prec,
-            "Z (nm)": z_prec,
+            "x": x_prec,
+            "y": y_prec,
+            "z": z_prec,
         }
     )
 
     # Use Arial font and set global font sizes
     plt.rcParams.update(
         {
-            "axes.titlesize": 16,  # title size
-            "axes.labelsize": 14,  # x/y label size
-            "xtick.labelsize": 12,  # x tick label size
-            "ytick.labelsize": 12,  # y tick label size
-            "legend.fontsize": 12,  # legend label size
+            "axes.titlesize": 12,  # title size
+            "axes.labelsize": 11,  # x/y label size
+            "xtick.labelsize": 9,  # x tick label size
+            "ytick.labelsize": 9,  # y tick label size
+            "legend.fontsize": 11,  # legend label size
         }
     )
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(4, 2.4)) # 10, 6
     plot = sns.histplot(
         data=pd_df,
         element="step",
@@ -92,9 +92,10 @@ for protein in ["ACTN2", "Z1Z2", "ZASP6"]:
     legend.set_title(None)
     plt.xlim(0, 40)
     plt.xticks([0, 5, 10, 15, 20, 25, 30, 35, 40])
-    plt.xlabel("Localisation precision")
+    plt.xlabel("Localisation precision / nm")
     plt.ylabel("Localisation count")
-    plt.title(f"Localisation precision for {protein} (pre-filtering)")
+    #plt.title(f"Localisation precision for {protein} (pre-filtering)")
+    plt.title(f"{protein}")
     output_loc = os.path.join(
         output_folder, f"figure_k1/{protein}.svg"
     )
@@ -102,14 +103,28 @@ for protein in ["ACTN2", "Z1Z2", "ZASP6"]:
 
     # generate table k1
     table_protein.append(protein)
-    table_xprec_med.append(pd_df.median()["X (nm)"].round(1))
-    table_yprec_med.append(pd_df.median()["Y (nm)"].round(1))
-    table_zprec_med.append(pd_df.median()["Z (nm)"].round(1))
+
+    xprec_mean = pd_df.mean()["x"].round(1)
+    xprec_std = pd_df.std(ddof=0)["x"].round(1)
+    table_xprec_mean.append(f"{xprec_mean} " +  u"\u00B1" + f" {xprec_std}")
+
+    yprec_mean = pd_df.mean()["y"].round(1)
+    yprec_std = pd_df.std(ddof=0)["y"].round(1)
+    table_yprec_mean.append(f"{yprec_mean} " +  u"\u00B1" + f" {yprec_std}")
+
+    zprec_mean = pd_df.mean()["z"].round(1)
+    zprec_std = pd_df.std(ddof=0)["z"].round(1)
+    table_zprec_mean.append(f"{zprec_mean} " +  u"\u00B1" + f" {zprec_std}")
+
     table_nzdisks.append(len(files))
-    table_locsperzdisk_med.append(np.median(locs_per_zdisk))
-    table_zdisksize_x_med.append(np.median(zdisksize_x).round(1))
-    table_zdisksize_y_med.append(np.median(zdisksize_y).round(1))
-    table_zdisksize_z_med.append(np.median(zdisksize_z).round(1))
+
+    table_locsperzdisk_mean.append(f"{int(np.mean(locs_per_zdisk).round(0))} " + u"\u00B1" + f" {int(np.std(locs_per_zdisk, ddof=0).round(0))}")
+
+    table_zdisksize_x_mean.append(f"{np.mean(zdisksize_x).round(1)} " + u"\u00B1" + f" {np.std(zdisksize_x, ddof=0).round(1)}")
+
+    table_zdisksize_y_mean.append(f"{np.mean(zdisksize_y).round(1)} " + u"\u00B1" + f" {np.std(zdisksize_y, ddof=0).round(1)}")
+
+    table_zdisksize_z_mean.append(f"{np.mean(zdisksize_z).round(1)} " + u"\u00B1" + f" {np.std(zdisksize_z, ddof=0).round(1)}")
 
     #print("Mean precision (pre-filtering)")
     #print(pd_df.mean())
@@ -120,17 +135,17 @@ for protein in ["ACTN2", "Z1Z2", "ZASP6"]:
 df = pd.DataFrame(
     {"Protein": table_protein,
      "N z disks": table_nzdisks,
-     "Med z disk size in x": table_zdisksize_x_med,
-     "Med z disk size in y": table_zdisksize_y_med,
-     "Med z disk size in z": table_zdisksize_z_med,
-     "Med locs per z disk": table_locsperzdisk_med,
-     "Median x precision": table_xprec_med,
-     "Median y precision": table_yprec_med,
-     "Median z precision": table_zprec_med,
+     "mean z disk size in x": table_zdisksize_x_mean,
+     "mean z disk size in y": table_zdisksize_y_mean,
+     "mean z disk size in z": table_zdisksize_z_mean,
+     "mean locs per z disk": table_locsperzdisk_mean,
+     "mean x precision": table_xprec_mean,
+     "mean y precision": table_yprec_mean,
+     "mean z precision": table_zprec_mean,
      }  
 )
 
 output_loc = os.path.join(
         output_folder, f"table_k1.csv"
     )
-df.to_csv(output_loc, index=False)
+df.to_csv(output_loc, index=False,encoding="utf-8-sig")
